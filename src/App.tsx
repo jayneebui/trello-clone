8bc2e412f5b828f2227c31ec3d60bc9f72e580d5
Jaynee Bui <pbui2@ucsc.edu>
2024-03-04 20:41:43 -0800

    made custom hooks and states

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import {Column} from './Column';
 import {AddNewItem} from './AddNewItem';
 import {AppContainer} from './styles';
+import {useAppState} from './state/AppStateContext';
 
 export const App = () => {
+  const {lists} = useAppState();
+
   return (
     <AppContainer>
-      <Column text='Todo:' />
+      {lists.map((list) => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
       <AddNewItem toggleButtonText='+ Add another list' onAdd={console.log} />
     </AppContainer>
   );
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,9 +1,10 @@
-import { ColumnContainer, ColumnTitle, CardContainer  } from "./styles"
+import {ColumnContainer, ColumnTitle, CardContainer} from './styles';
 
 type CardProps = {
-   text: string
-}
+  text: string;
+  id: string;
+};
 
-export const Card = ({ text }: CardProps) => {
-   return <CardContainer>{text}</CardContainer>
-}
+export const Card = ({text}: CardProps) => {
+  return <CardContainer>{text}</CardContainer>;
+};
